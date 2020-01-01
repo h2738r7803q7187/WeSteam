a5169dc6fe4c15a20f695c0582ad3e4942740f06
游学者琪琪 <277662392@qq.com>
2020-01-01 18:12:26 +0800

    wasd移动

---
+++ Project2020/Assets/Scripts/Controler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Player Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            int x = Mathf.FloorToInt(Input.mousePosition.x / 100);
+            int y = Mathf.FloorToInt(Input.mousePosition.y / 100);
+            Debug.Log("点击" + x + y);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Player.MoveUp();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Player.MoveLeft();
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Player.MoveDown();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Player.MoveRight();
+        }
+    }
+}
---
+++ Project2020/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void MoveUp()
+    {
+        if (transform.localPosition.y >= 400) return;
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 100);
+    }
+    public void MoveDown()
+    {
+        if (transform.localPosition.y <= -500) return;
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 100);
+    }
+
+    public void MoveLeft()
+    {
+        if (transform.localPosition.x <= -500) return;
+        transform.localPosition = new Vector3(transform.localPosition.x - 100, transform.localPosition.y);
+    }
+
+    public void MoveRight()
+    {
+        if (transform.localPosition.x >= 400) return;
+        transform.localPosition = new Vector3(transform.localPosition.x + 100, transform.localPosition.y);
+    }
+}
