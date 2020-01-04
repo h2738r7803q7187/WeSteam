06259fba1b52a07ee487466161d353bb4bb59ab5
游学者琪琪 <277662392@qq.com>
2020-01-04 17:00:55 +0800

    建筑UI

---
+++ Project2020/Assets/Scripts/MainUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+    public Button BuildingBtn;
+    public Button BuildingBack;
+    
+    public Transform BuildingUI;
+    public Transform BuildingContent;
+    public Transform BuildingItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildingBtn.onClick.AddListener(ShowBuildingUI);
+        BuildingBack.onClick.AddListener(HideBuildingUI);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowBuildingUI()
+    {
+        BuildingUI.gameObject.SetActive(true);
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(BuildingItem, BuildingContent).gameObject.SetActive(true);
+        }
+    }
+
+    void HideBuildingUI()
+    {
+        BuildingUI.gameObject.SetActive(false);
+        Tools.ClearChildren(BuildingContent);
+    }
+
+
+}
---
+++ Project2020/Assets/Scripts/Tools.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools : MonoBehaviour
+{
+    public static void ClearChildren(Transform t)
+    {
+        for(int i = 0; i < t.childCount; i++)
+        {
+            Destroy(t.GetChild(i).gameObject);
+        }
+    }
+}
