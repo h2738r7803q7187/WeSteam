aadf9ca04bfc61b60f6b855af583a9ead2b8072f
游学者琪琪 <277662392@qq.com>
2020-03-12 22:44:37 +0800

    六边形

---
+++ Project2020/Assets/Scripts/HexagonMesh.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexagonMesh : MonoBehaviour
+{
+    Mesh mesh;
+    public float radius;
+    float g3 = Mathf.Sqrt(3);
+
+    private void OnEnable()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh.name = "正六边形";
+        mesh.vertices = new Vector3[] {
+            new Vector3(1, 0, 0),
+            new Vector3(0.5f, 0, -g3/2),
+            new Vector3(-0.5f, 0, -g3/2),
+            new Vector3(-1, 0, 0),
+            new Vector3(-0.5f, 0, g3/2),
+            new Vector3(0.5f, 0, g3/2),
+        };
+        mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 5 };
+        mesh.uv = new Vector2[] {
+            new Vector2(1,0.5f),
+            new Vector2(0.75f,0.5f-g3/4),
+            new Vector2(0.25f,0.5f-g3/4),
+            new Vector2(0,0.5f),
+            new Vector2(0.25f,0.5f+g3/4),
+            new Vector2(0.75f,0.5f-g3/4)
+        };
+    }
+}
---
+++ Project2020/Assets/Scripts/MapInit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class MapInit : MonoBehaviour
+{
+    public HexagonMesh grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random random = new System.Random(1);
+        for (int i = -50; i < 50; i++)
+        {
+            for (int j = -50; j < 50; j++)
+            {
+                if (random.Next(0, 3) < 1)
+                {
+                    HexagonGrid H = Instantiate(grid.gameObject).GetComponent<HexagonGrid>();
+                    H.x = i;
+                    H.y = j;
+                }
+            }
+        }
+    }
+}
