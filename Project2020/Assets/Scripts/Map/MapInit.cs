1e68f4a21d5e2511fca2e10c8e7a7e702e318cb8
游学者琪琪 <277662392@qq.com>
2020-03-14 17:24:48 +0800

    三合一拼图

--- Project2020/Assets/Scripts/Map/MapInit.cs
+++ Project2020/Assets/Scripts/Map/MapInit.cs
@@ -9,9 +9,9 @@ public class MapInit : MonoBehaviour
     void Start()
     {
         System.Random random = new System.Random(1);
-        for (int i = -50; i < 50; i++)
+        for (int i = -20; i < 20; i++)
         {
-            for (int j = -50; j < 50; j++)
+            for (int j = -20; j < 20; j++)
             {
                 if (random.Next(0, 3) < 1)
                 {
---
+++ Project2020/Assets/Scripts/Player/PlayerMesh.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMesh : MonoBehaviour
+{
+    Mesh mesh1, mesh2, mesh3;
+    public Texture t1, t2, t3;
+    Transform child1, child2, child3;
+    readonly int[] triangles = new int[] { 0, 2, 1, 1, 2, 3 };
+
+    void Start()
+    {
+        child1 = transform.GetChild(0);
+        child2 = transform.GetChild(1);
+        child3 = transform.GetChild(2);
+        mesh1 = child1.GetComponent<MeshFilter>().mesh;
+        mesh2 = child2.GetComponent<MeshFilter>().mesh;
+        mesh3 = child3.GetComponent<MeshFilter>().mesh;
+        mesh1.vertices = new Vector3[]
+        {
+            new Vector3(0,0,0.67f),
+            new Vector3(1,0,0.67f),
+            new Vector3(0,0,1),
+            new Vector3(1,0,1),
+        };
+        mesh2.vertices = new Vector3[]
+        {
+            new Vector3(0,0,0.33f),
+            new Vector3(1,0,0.33f),
+            new Vector3(0,0,0.67f),
+            new Vector3(1,0,0.67f),
+        };
+        mesh3.vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(1,0,0),
+            new Vector3(0,0,0.33f),
+            new Vector3(1,0,0.33f),
+        };
+        mesh1.triangles = triangles;
+        mesh2.triangles = triangles;
+        mesh3.triangles = triangles;
+        mesh1.uv = new Vector2[]{
+            new Vector2(0,0.67f),
+            new Vector2(1,0.67f),
+            new Vector2(0,1),
+            new Vector2(1,1),
+        };
+        mesh2.uv = new Vector2[]{
+            new Vector2(0,0.33f),
+            new Vector2(1,0.33f),
+            new Vector2(0,0.67f),
+            new Vector2(1,0.67f),
+        };
+        mesh3.uv = new Vector2[]{
+            new Vector2(0,0),
+            new Vector2(1,0),
+            new Vector2(0,0.33f),
+            new Vector2(1,0.33f),
+        };
+    }
+
+    void Update()
+    {
+        SetTex();
+    }
+
+    void SetTex()
+    {
+        child1.GetComponent<MeshRenderer>().material.mainTexture = t1;
+        child2.GetComponent<MeshRenderer>().material.mainTexture = t2;
+        child3.GetComponent<MeshRenderer>().material.mainTexture = t3;
+    }
+}
